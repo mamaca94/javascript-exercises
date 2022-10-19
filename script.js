c3cfdb633e5cae7edb27756106cc5eeb0bf21666
mamaca94 <giobarca1994@gmail.com>
2022-10-19 16:36:06 +0400

    add event listeners to buttons

--- script.js
+++ script.js
@@ -8,6 +8,20 @@ const btnCloseModal = document.querySelector(".close-modal");
 
 const btnShowModal = document.querySelectorAll(".show-modal");
 
+const openModal = function () {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
 for (let i = 0; i < btnShowModal.length; i++) {
-  console.log(btnShowModal[i].textContent);
+  btnShowModal[i].addEventListener("click", openModal);
 }
+
+btnCloseModal.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
