08cd31922193d62edadc72821296a0b32ec4970d
mamaca94 <giobarca1994@gmail.com>
2022-10-26 18:59:21 +0400

    nested arrays

--- destructuring arrays/main.js
+++ destructuring arrays/main.js
@@ -6,4 +6,25 @@ const restaurant = {
   categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
   startMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
   mainMenu: ["Pizza", "Pasta", "Risotto"],
+
+  order: function (starterIndex, mainIndex) {
+    return [this.startMenu[starterIndex], this.mainMenu[mainIndex]];
+  },
 };
+
+let [main, , secondary] = restaurant.categories;
+
+console.log(main, secondary);
+
+[main, secondary] = [secondary, main];
+
+const [starter, mainCourse] = restaurant.order(2, 0);
+console.log(starter, mainCourse);
+
+// nested destructuring
+const nested = [2, 4, [5, 6]];
+
+const [i, , [j, k]] = nested;
+console.log(i, j, k);
+
+// default values
